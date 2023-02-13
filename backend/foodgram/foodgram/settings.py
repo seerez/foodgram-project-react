7a3b08d9db1cb9138f1fc362ef346248e3a2468b
seerez <serezha.mdinaradze@bk.ru>
2023-02-13 21:19:48 +0300

    fix

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = ')*+i_9dxi7nfbd+^z(x_^-!e$ynf3lt92b-apb0m$bgr++gdrw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
