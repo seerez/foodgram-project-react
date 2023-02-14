a74cf172f2efbf69835b56854b9ae45a21afff60
seerez <serezha.mdinaradze@bk.ru>
2023-02-14 19:20:58 +0300

    fix

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('recipes.urls')),
     path('api/', include('tags.urls')),
