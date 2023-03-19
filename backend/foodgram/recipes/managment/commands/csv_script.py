089ab5f6a1f1119de7267d89ffb91d1d46cba91d
seerez <serezha.mdinaradze@bk.ru>
2023-03-19 14:38:35 +0300

    fix import db

--- backend/foodgram/recipes/managment/commands/csv_script.py
+++ backend/foodgram/recipes/managment/commands/csv_script.py
@@ -1,27 +1,27 @@
-import os
+import os.path
 import csv
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
+
 from recipes.models import Ingredient
 
-
-def read_csv(name_file):
-    """Считывает данные из csv и возвращает список строк таблицы"""
-    path = os.path.join('../../data/', name_file)
-    with open(path, encoding='utf-8') as csv_file:
-        reader = csv.reader(csv_file, delimiter=',')
-        return list(reader)
+DATA_ROOT = os.path.join(settings.BASE_DIR, 'data')
 
 
-def load_data(model, name_file):
-    """
-    Загрузка данных по имени модели.
-    Не загружает данные во вспомогательную таблицу
-    со связью многие ко многим
-    """
-    table = read_csv(name_file)
-    model.objects.bulk_create(
-        model(idx, *row) for idx, row in enumerate(table)
-        )
+class Command(BaseCommand):
 
-#load_data(Ingredient, 'ingredients.csv')
+    def hadle(self, *args, **options):
+        self.import_ingredient('ingredients.csv')
+        """Считывает данные из csv и возвращает список строк таблицы"""
+
+
+    def import_ingredients(self, file):
+        file_path = os.path.join(DATA_ROOT, file)
+        with open(file_path, newline='', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                status, created = Ingredient.objects.update_or_create(
+                    name=row[0],
+                    measurement_unit=row[1]
+                )
